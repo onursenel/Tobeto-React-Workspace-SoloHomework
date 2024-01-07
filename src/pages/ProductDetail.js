24dfa19953157c13ffb357c44c54fc8ef4af1c8a
Onur Şenel <onrsenel@gmail.com>
2024-01-07 21:26:28 +0300

    25.12.2023 (ödevi) Yapıldı

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -21,10 +21,10 @@ export default function ProductDetail() {
                 <img src="..." class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">{products.modelName}</h5>
-                    <p class="card-text">Aracın özellikleri</p>
+                    <p class="card-text">Aracın özellikleri </p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">An i</li>
                     <li class="list-group-item">A second item</li>
                     <li class="list-group-item">A third item</li>
                 </ul>
